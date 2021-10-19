e54b34bb3a75f34ef7ab089849453a98b609aeaf
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2021-10-19 11:01:32 -0400

    Build main structure of the featured view

--- RecipeListApp/View/FeatureView.swift
+++ RecipeListApp/View/FeatureView.swift
@@ -11,13 +11,68 @@ struct FeatureView: View {
     @EnvironmentObject private var recipeModel: RecipeModel
     
     var body: some View {
-        // TODO: Implement the feature view
-        Text("IMPLEMENT ME!")
+        VStack (alignment: .leading, spacing: 0) {
+            // MARK: Title
+            Text("Featured Recipes")
+                .bold()
+                .padding(.leading)
+                .padding()
+                .font(.largeTitle)
+            
+            // MARK: Dish Card
+            GeometryReader { geo in
+                TabView {
+                    ForEach (0 ..< recipeModel.recipes.count) { index in
+                        let recipe = recipeModel.recipes[index]
+                        // Only show featured recipe(s)
+                        if recipe.featured {
+                            // Define our own faded black shadow
+                            let shadowColor = Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5)
+                            
+                            ZStack {
+                                Rectangle()
+                                    .foregroundColor(.white)
+                                
+                                VStack (spacing: 0) {
+                                    Image(recipe.image)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .clipped()
+                                    Text(recipe.name)
+                                        .padding(5)
+                                }
+                            }
+                            .frame(width: geo.size.width/8*7, height: geo.size.height - 100)
+                            .cornerRadius(15)
+                            .shadow(color: shadowColor, radius: 10, x: -5, y: 5)
+                        }
+                    }
+                }
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            }
+            
+            // MARK: Prep Time and Highlights
+            VStack (alignment: .leading, spacing: 10) {
+                // TODO: Implement these details based on card being displayed above
+                Text("Preparation Time:")
+                    .font(.headline)
+                Text("** IMPLEMENT **")
+                Text("Highlights:")
+                    .font(.headline)
+                Text("** IMPLEMENT **")
+            }
+            .padding([.leading, .bottom])
+        }
     }
 }
 
 struct FeatureView_Previews: PreviewProvider {
     static var previews: some View {
+        // Added for testing data
+        let recipeModel = RecipeModel.instance
+        
         FeatureView()
+            .environmentObject(recipeModel)
     }
 }
